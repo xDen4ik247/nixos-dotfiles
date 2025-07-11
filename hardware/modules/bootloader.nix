5557243c2cb7a197be50fdb3df08db9611268744
gemakfy <gemakfy@gmail.com>
2025-07-11 23:08:32 +0300

    update bootloader

--- hardware/modules/bootloader.nix
+++ hardware/modules/bootloader.nix
@@ -22,21 +22,10 @@
         enable = true;
         efiSupport = true;
         useOSProber = true;
+
         gfxmodeEfi = "2560x1440";
         gfxpayloadEfi = "keep";
-
-        theme = pkgs.stdenv.mkDerivation {
-          pname = "MilkGrub";
-          version = "1.0";
-          src = pkgs.fetchFromGitHub {
-            owner = "gemakfy";
-            repo = "MilkGrub";
-            rev = "9bb508ae1486b46466aaa4a72df8fbd9f240db66";
-            hash = "sha256-zX0xl3CNVWRQB0CZi/PtIm68mDe/z0nGj2x68B1Z7/A=";
-          };
-          installPhase = "cp -r . $out";
-        };
-        splashImage = "${config.boot.loader.grub.theme}/assets/background.jpg";
+        milk-theme.enable = true;
       };
     };
   };
