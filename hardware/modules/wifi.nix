f12c2f44d0442061e3a8b39663d7b5055d2332c5
xDen4ik247 <d.pilyugin2009@gmail.com>
2025-09-11 20:17:59 +0300

    Update wifi.nix

--- hardware/modules/wifi.nix
+++ hardware/modules/wifi.nix
@@ -2,13 +2,14 @@
 
 {
   boot = {
-    kernelModules = [ "rtl8852bu" ];
+    kernelModules = [ "8852bu" ];
     extraModulePackages = [ config.boot.kernelPackages.rtl8852bu ];
   };
-  
+
   environment.systemPackages = [ pkgs.usb-modeswitch ];
 
   services.udev.extraRules = ''
-    ATTR{idVendor}=="0bda", ATTR{idProduct}=="1a2b", RUN+="${lib.getExe pkgs.usb-modeswitch} -K -v 0bda -p 1a2b"
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="1a2b", \
+      RUN+="${lib.getExe pkgs.usb-modeswitch} -K -v 0bda -p 1a2b"
   '';
 }
