bb5a8f6b567edd0e54f0360e371781953bade2a3
xDen4ik247 <d.pilyugin2009@gmail.com>
2025-09-11 20:26:27 +0300

    Update wifi.nix

--- hardware/modules/wifi.nix
+++ hardware/modules/wifi.nix
@@ -1,20 +1,8 @@
 { config, pkgs, lib, ... }:
 
-let
-  rtl8852buPatched = pkgs.rtl8852bu.overrideAttrs (old: rec {
-    postPatch = (old.postPatch or "") + ''
-      grep -R -l "from_timer" . >/tmp/_fm || true
-      if [ -s /tmp/_fm ]; then
-        while IFS= read -r f; do
-          sed -i 's/from_timer/timer_container_of/g' "$f" || true
-        done </tmp/_fm
-      fi
-    '';
-  });
-in
 {
-  boot.kernelModules = [ "8852bu" ];
-  boot.extraModulePackages = [ rtl8852buPatched ];
+  boot.kernelModules = [ "rtl8852bu" ];
+  boot.extraModulePackages = [ rtl8852bu ];
 
   environment.systemPackages = [ pkgs.usb-modeswitch ];
 
